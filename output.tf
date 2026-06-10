9115f83f34c5b39580c046d20aacde39a4ac9c0c
prasanna70-cyber <prasannadeepthi70@gmail.com>
2026-06-10 15:07:32 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,21 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main_vpc.id
+}
+
+output "webserver1_public_ip" {
+  description = "Web Server 1 Public IP"
+  value       = aws_instance.webserver1.public_ip
+}
+
+output "webserver2_public_ip" {
+  description = "Web Server 2 Public IP"
+  value       = aws_instance.webserver2.public_ip
+}
+
+output "alb_dns_name" {
+  description = "Application Load Balancer DNS Name"
+  value       = aws_lb.alb.dns_name
+}
+}
+
